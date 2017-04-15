57998b29a00142d67261c3f7b9f823d31421d811
Yuichi Sasaki <ysasaki6023@gmail.com>
2017-04-16 00:49:13 +0900

    added save

--- test.py
+++ test.py
@@ -26,10 +26,6 @@ def animate(step):
         gmm.startNewEpoch()
         print()
 
-        # for log
-        #win += 1
-
-        #print("WIN: {:03d}/{:03d} ({:.1f}%)".format(win, win + lose, 100 * win / (win + lose)))
 
     else:
         state_t = state_t_1
@@ -41,8 +37,6 @@ def animate(step):
 
     # observe environment
     state_t_1, reward_t, rewardDrop, terminal = gmm.observe()
-    #screen = all_state_t_1[0] + all_state_t_1[1]
-    #state_t_1 = train.flatten(all_state_t_1)
 
     # animate
     img.set_array(state_t_1)
@@ -56,11 +50,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model_path")
     parser.add_argument("-s", "--save", dest="save", action="store_true")
+    parser.add_argument("-l", "--log", dest="log",default=None)
+    parser.add_argument("-n", "--logLength", dest="logLength",default=1000,type=int)
     parser.set_defaults(save=False)
     args = parser.parse_args()
 
     # environmet, agent
-    #gmm = gameMgr.tetris(12,6)
     gmm = gameMgr.tetris(20,10)
     agt = agent.agent(gmm.getActionList(),gmm.getScreenSize(),gmm.getNextBlockSize(),n_batch=1,learning_rate=0, discountRate=0, saveFreq=0, saveFolder=None, memoryLimit=0.05)
     agt.load(args.model_path)
@@ -68,13 +63,16 @@ if __name__ == "__main__":
     # variables
     win, lose = 0, 0
     state_t_1, reward_t, rewardDrop, terminal = gmm.observe()
-    #screen = all_state_t_1[0] + all_state_t_1[1]
-    #state_t_1 = train.flatten(all_state_t_1)
 
     # animate
     fig = plt.figure(figsize=(gmm.getScreenSize()[0]/2,gmm.getScreenSize()[1]/2))
     fig.canvas.set_window_title("TeTris")
     img = plt.imshow(state_t_1, interpolation="none", cmap="gray")
-    ani = animation.FuncAnimation(fig, animate, init_func=init, interval=(1000 / frame_rate), blit=True)
+
+    if args.log:
+        ani = animation.FuncAnimation(fig, animate, init_func=init, interval=(1000 / frame_rate), blit=True,save_count=args.logLength)
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=30, metadata=dict(artist='Yuichi Sasaki',title="tetris A3C"))
+        ani.save(args.log+".mp4", writer=writer)
 
     plt.show()
